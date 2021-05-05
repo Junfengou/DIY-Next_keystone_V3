4dc6c6ad72f5dd4289a92078ee0bac9d8a4efce4
Junfeng ou <junfengou@gmail.com>
2021-05-04 22:03:39 -0500

    testing deployment

--- frontend/config.js
+++ frontend/config.js
@@ -1,5 +1,5 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
 // export const endpoint = `http://localhost:3000/api/graphql`;
-export const endpoint = `https://diy-backend.herokuapp.com/`;
-export const prodEndpoint = `https://diy-backend.herokuapp.com/`;
+export const endpoint = `https://diy-backend.herokuapp.com/api/graphql`;
+export const prodEndpoint = `https://diy-backend.herokuapp.com/api/graphql`;
 export const perPage = 4;
